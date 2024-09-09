dcb75831d6fb976082cb9b0c8c796e16f905b90b
mayya0 <mayya10501@gmail.com>
2024-09-09 19:07:18 +0300

    Create name2.c

---
+++ name2.c
@@ -0,0 +1,11 @@
+#include <locale.h>
+void main()
+{
+	setlocale(LC_CTYPE, "RUS");
+
+	puts("   __   .    __   __    __   __      ");
+	puts("  |  | /|   |  | |_    |  | |__      ");
+	puts("  |  |  |   |  |   |   |  | |  |     ");
+	puts("   --   ' .  --  --  .  --   -- .    ");
+
+}
