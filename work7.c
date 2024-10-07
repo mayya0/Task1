491d046b9907e45ea92729ec85e721869c8386a0
mayya0 <mayya10501@gmail.com>
2024-10-07 21:34:40 +0300

    work7.c

--- work7.c
+++ work7.c
@@ -23,74 +23,74 @@ int task1()
 	scanf("%c", &c);//считывание с консоли
 	switch (c)
 	{
-	case '0': 
-	case '1': 
-	case '2': 
+	case '0':
+	case '1':
+	case '2':
 	case '3':
-	case '4': 
-	case '5': 
-	case '6': 
+	case '4':
+	case '5':
+	case '6':
 	case '7':
-	case '8': 
+	case '8':
 	case '9':
 		printf("Введено число\n");
 		break;
-	case 'A': 
-	case 'B': 
-	case 'C': 
+	case 'A':
+	case 'B':
+	case 'C':
 	case 'D':
-	case 'E': 
-	case 'F': 
-	case 'G': 
+	case 'E':
+	case 'F':
+	case 'G':
 	case 'H':
-	case 'I': 
-	case 'J': 
-	case 'K': 
+	case 'I':
+	case 'J':
+	case 'K':
 	case 'L':
-	case 'M': 
-	case 'N': 
-	case 'O': 
+	case 'M':
+	case 'N':
+	case 'O':
 	case 'P':
-	case 'Q': 
-	case 'R': 
-	case 'S': 
+	case 'Q':
+	case 'R':
+	case 'S':
 	case 'T':
-	case 'U': 
-	case 'V': 
-	case 'W': 
+	case 'U':
+	case 'V':
+	case 'W':
 	case 'X':
-	case 'Y': 
+	case 'Y':
 	case 'Z':
-	case 'a': 
-	case 'b': 
-	case 'c': 
+	case 'a':
+	case 'b':
+	case 'c':
 	case 'd':
-	case 'e': 
-	case 'f': 
-	case 'g': 
+	case 'e':
+	case 'f':
+	case 'g':
 	case 'h':
-	case 'i': 
-	case 'j': 
-	case 'k': 
+	case 'i':
+	case 'j':
+	case 'k':
 	case 'l':
-	case 'm': 
-	case 'n': 
-	case 'o': 
+	case 'm':
+	case 'n':
+	case 'o':
 	case 'p':
-	case 'q': 
-	case 'r': 
-	case 's': 
+	case 'q':
+	case 'r':
+	case 's':
 	case 't':
-	case 'u': 
-	case 'v': 
-	case 'w': 
+	case 'u':
+	case 'v':
+	case 'w':
 	case 'x':
-	case 'y': 
+	case 'y':
 	case 'z':
-		printf("'Введена цифра \n");
+		printf("\n Введена цифра \n");
 		break;
 	default:
-		printf("Веден символ\n");
+		printf("\n Веден символ\n");
 	}
 
 	return 0;
@@ -101,30 +101,30 @@ int task2()
 {
 	char c;
 	float x, y;
-	printf("Введите операцию и операнды ");
+	printf("\n Введите операцию и операнды ");
 	scanf("%f %c %f", &x, &c, &y);
 	switch (c)
 	{
 	case '+':
-		printf("x+y = %f", x+y);
+		printf("x+y = %f", x + y);
 		break;
 	case '-':
-		printf("x-y = %f", x-y);
+		printf("x-y = %f", x - y);
 		break;
 	case '/':
-		printf("x/y = %f", x/y);
+		printf("x/y = %f", x / y);
 		break;
 	case '*':
-		printf("x*y = %f", x*y);
+		printf("x*y = %f", x * y);
 		break;
 	case '^':
-		printf("x^y = %f", pow(x,y));
+		printf("x^y = %f", pow(x, y));
 		break;
 	case 'sin':
 		printf("sin x(y) = %f", sin(x, y));
 		break;
 	default:
-		printf("ошибка\n");
+		printf("\n ошибка\n");
 	}
 
 }
@@ -132,16 +132,44 @@ int home()
 
 {
 	int chas;
-	printf("Который час");
+	printf("\n Который у вас час \n");
 	scanf("%d", &chas);
 	switch (chas)
 	{
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	case '10':
+	case '11':
+	case '12':
+		printf("\nДоброго утра");
+		break;
+	case '13':
+	case '14':
+	case '15':
+	case '16':
+	case '17':
+		printf("\nДоброго дня");
+		break;
+	case '18':
+	case '19':
+	case '20':
+	case '21':
+	case '22':
+		printf("\nДоброго вечера");
+		break;
+	case '23':
 	case '0':
-		printf("Доброй ночи");
+	case '1':
+	case '2':
+		printf("\nСпокойной ночи");
 		break;
 
 	default:
-		printf("ошибка\n");
+		printf("\nошибка\n");
 	}
 
 }
