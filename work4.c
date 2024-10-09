9678f5f454d61e28425dc31d2402efea2df18905
mayya0 <mayya10501@gmail.com>
2024-10-09 20:56:09 +0300

    work4.c

--- work4.c
+++ work4.c
@@ -23,26 +23,26 @@ int task1()
 	int i;
 	float f;
 	double d;
-	
+
 	puts("введите символ");
 	scanf("%c", &c);
-	
+
 	puts("введите число");
 	scanf("%d", &i);
-	
+
 	puts("введите число");
 	scanf("%f", &f);
-	
+
 	puts("введите число");
 	scanf("%le", &d);
-	
+
 	printf("%c %d %f %le\n", c, i, f, d);
-	
+
 	printf("Целая часть числа - %d Дробная часть числа - %f\n", (int)f, f - (int)f);
-	
+
 	printf("%x %d\n", c, c);
-	
-	float b = 1/(float)i;
+
+	float b = 1 / (float)i;
 	printf("%f\n", b);
 
 }
@@ -51,7 +51,7 @@ int task2()
 	int a = 11;
 	int b = 3;
 
-	printf("%d,%f,%le \n", a/b, (float)a/b, (double)a/b);
+	printf("%d,%f,%le \n", a / b, (float)a / b, (double)a / b);
 
 }
 int task3()
@@ -59,30 +59,26 @@ int task3()
 	int n;
 	puts("Введите n\n");
 	scanf("%d", &n);
-	printf("Последняя цифра %d, первая - %d, сумма цифр равна %d\n", n%10, n/100, n%10+n/100+n/10%10);
+	printf("Последняя цифра %d, первая - %d, сумма цифр равна %d\n", n % 10, n / 100, n % 10 + n / 100 + n / 10 % 10);
 	printf("Число наоборот: %d%d%d\n", n % 10, n / 10 % 10, n / 100);
 
 }
-int home()
+int main()
 {
-	/* Клиент внес в банк R рублей. Каждый месяц эта сумма увеличивается на W процентов.
-	Cколько будет у клиента денег через два месяца, три месяца, через полгода? */
-
-	float R,W;
+	float R, W;
 	puts("Введите сумму, внесенную в банк\n");
-	scanf("%f",&R);
+	scanf("%f", &R);
 	puts("Введите процент\n");
-	scanf("%f",&W);
+	scanf("%f", &W);
 
-	float summ1month = R+ W*(R/100);
-	float summ2month = summ1month + W*(summ1month/100);
-	float summ3month = summ2month + W*(summ2month/100);
-	float summ4month = summ3month + W*(summ3month/100);
-	float summ5month = summ4month + W*(summ4month/100);
-	float summ6month = summ5month + W*(summ5month/100);
+	float s1 = R + W * (R / 100);
+	float s2 = s1 + W * (s1 / 100);
+	float s3 = s2 + W * (s2 / 100);
+	float s4 = s3 + W * (s3 / 100);
+	float s5 = s4 + W * (s4 / 100);
+	float s6 = s5 + W * (s5 / 100);
 
-	printf("Через два месяца у клиента будет %f рублей\n", summ2month);
-	printf("Через три месяца у клиента будет %f рублей\n", summ3month);
-	printf("Через пол года у клиента будет %f рублей\n", summ6month);
-
+	printf("Через два месяца у клиента будет %f рублей\n", s2);
+	printf("Через три месяца у клиента будет %f рублей\n", s3);
+	printf("Через пол года у клиента будет %f рублей\n", s6);
 }
